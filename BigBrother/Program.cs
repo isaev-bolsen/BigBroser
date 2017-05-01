365e3e05cd7327f48e9bfac94028583c377fd9ae
isaev-bolsen <al_mamistov@mail.ru>
2017-05-01 10:05:59 +0300

    MK1 : collect except inial

--- BigBrother/Program.cs
+++ BigBrother/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BigBrother
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            IEnumerable<Process> initial = Process.GetProcesses();
+            List<Process> collected = new List<Process>();
+
+            DateTime startTime = DateTime.Now;
+            TimeSpan interval = TimeSpan.FromMinutes(1);
+
+            while (DateTime.Now - startTime < interval) collected.AddRange(Process.GetProcesses().Except(initial));
+
+            Console.WriteLine(string.Join(Environment.NewLine, collected.Select(p => p.StartInfo.FileName)));
         }
     }
 }
