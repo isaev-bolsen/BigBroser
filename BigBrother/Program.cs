2353663f1d60d0792e396e72860478f016df8df5
isaev-bolsen <al_mamistov@mail.ru>
2017-05-01 10:16:30 +0300

    ProcessComparer

--- BigBrother/Program.cs
+++ BigBrother/Program.cs
@@ -7,6 +7,12 @@ namespace BigBrother
 {
     class Program
     {
+        public class ProcessComparer : IEqualityComparer<Process>
+        {
+            public static ProcessComparer Instance = new ProcessComparer();
+            public bool Equals(Process x, Process y) { return x.Id == y.Id; }
+            public int GetHashCode(Process obj) { return obj.Id; }
+        }
 
         static void Main(string[] args)
         {
@@ -14,11 +20,14 @@ namespace BigBrother
             List<Process> collected = new List<Process>();
 
             DateTime startTime = DateTime.Now;
-            TimeSpan interval = TimeSpan.FromMinutes(1);
+            TimeSpan interval = TimeSpan.FromMinutes(0.2);
 
-            while (DateTime.Now - startTime < interval) collected.AddRange(Process.GetProcesses().Except(initial));
+            while (DateTime.Now - startTime < interval)
+            {
+                collected.AddRange(Process.GetProcesses().Except(initial, ProcessComparer.Instance));
+            }
 
-            Console.WriteLine(string.Join(Environment.NewLine, collected.Select(p => p.StartInfo.FileName)));
+            Console.WriteLine(string.Join(Environment.NewLine, collected.Select(p => string.Join(" ", p.ProcessName, p.StartInfo.FileName))));
         }
     }
 }
