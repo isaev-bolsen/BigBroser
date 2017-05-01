3b0476f6fdbbbdf5901fb770b70cdc5a6ecb0d7d
isaev-bolsen <al_mamistov@mail.ru>
2017-05-01 10:49:05 +0300

    Silly way to get command line

--- BigBrother/Program.cs
+++ BigBrother/Program.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 
 namespace BigBrother
 {
     class Program
     {
+        public class ProcessInfo
+        {
+            const string query = "SELECT Name, CommandLine, ProcessId, Caption, ExecutablePath FROM Win32_Process WHERE ProcessId = ";
+
+            public readonly Process Process;
+            public readonly string CommandLine;
+
+            public ProcessInfo(Process process)
+            {
+                Process = process;
+                ManagementObject ManagementObject = new ManagementObjectSearcher(query + process.Id).Get().OfType<ManagementObject>().Single();
+                CommandLine = ManagementObject["CommandLine"].ToString();
+            }
+
+            public override string ToString()
+            {
+                return string.Join(" ", Process.ProcessName, CommandLine);
+            }
+        }
+
         public class ProcessComparer : IEqualityComparer<Process>
         {
             public static ProcessComparer Instance = new ProcessComparer();
@@ -14,21 +35,23 @@ namespace BigBrother
             public int GetHashCode(Process obj) { return obj.Id; }
         }
 
+
         static void Main(string[] args)
         {
-            IEnumerable<Process> collected = GetNewProcesses(TimeSpan.FromMinutes(1));
-            Console.WriteLine(string.Join(Environment.NewLine, collected.Select(p => string.Join(" ", p.ProcessName, p.StartInfo.FileName))));
+            IEnumerable<ProcessInfo> collected = GetNewProcesses(TimeSpan.FromMinutes(0.2));
+            Console.WriteLine(string.Join(Environment.NewLine, collected.ToString()));
         }
 
-        static IEnumerable<Process> GetNewProcesses(TimeSpan interval)
+        static IEnumerable<ProcessInfo> GetNewProcesses(TimeSpan interval)
         {
             DateTime startTime = DateTime.Now;
             IEnumerable<Process> initial = Process.GetProcesses();
-            List<Process> collected = new List<Process>();
+            List<ProcessInfo> collected = new List<ProcessInfo>();
 
             while (DateTime.Now - startTime < interval)
             {
-                collected.AddRange(Process.GetProcesses().Except(initial, ProcessComparer.Instance));
+                IEnumerable<Process> Current = Process.GetProcesses().Except(initial.Union(collected.Select(p => p.Process)), ProcessComparer.Instance);
+                collected.AddRange(Current.Select(p => new ProcessInfo(p));
             }
             return collected;
         }
